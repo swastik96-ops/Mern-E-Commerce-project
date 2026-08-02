b7eb0c59588d17b7e66fae2532c3535c57385811
swastik96-ops <swastik9672@gmail.com>
2026-08-03 00:00:54 +0530

    backend authentication and user APIs

--- backend/index.js
+++ backend/index.js
@@ -6,6 +6,7 @@ import cookieParser from "cookie-parser"
 
 //Utiles
 import connectDB from './config/db.js'
+import userRoutes from "./routes/userRoutes.js"
 
 const result = dotenv.config();
 const port = process.env.PORT || 5000
@@ -17,8 +18,6 @@ app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 app.use(cookieParser())
 
-app.get("/", (req,res) => {
-    res.send("Hello World");
-})
+app.use("/api/users", userRoutes);
 
 app.listen(port, () => console.log(`Server running on port: ${port}`));
