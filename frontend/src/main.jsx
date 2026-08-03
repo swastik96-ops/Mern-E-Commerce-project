5f949b966fd2c8fa7f4a199c86a1343303c4e7e7
swastik96-ops <swastik9672@gmail.com>
2026-08-03 09:55:22 +0530

    Basic frontend

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,47 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from "react-dom/client";
 import App from './App.jsx'
 import './index.css'
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import { Provider } from 'react-redux';
+import store from './redux/store.js';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import PrivateRoute from './components/PrivateRoute.jsx';
+
+import AdminRoute from './pages/Admin/AdminRoute.jsx';
+
+import Login from './pages/Auth/login.jsx'
+import Register from './pages/Auth/Register.jsx';
+
+import Profile from './pages/User/Profile.jsx';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+  <Route path='/' element= {<App />}>
+
+    <Route path='' element={<PrivateRoute />} >
+      <Route path='/profile' element={<Profile />} />
+    </Route>
+
+    <Route path = '/Login' element={<Login/>}/>
+    <Route path = "/register" element={<Register/>} />
+
+    <Route path='/admin' element={<AdminRoute/>}> 
+
+    </Route>
+
+  </Route>
+    )
+);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
+    <RouterProvider router={router} />
+  </Provider>
+  
+);
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,4 +5,9 @@ export default defineConfig({
   plugins: [
     tailwindcss(),
   ],
-})
+  server: {
+    proxy: {
+      "/api/": "http://localhost:5000",
+    },
+  },
+});
